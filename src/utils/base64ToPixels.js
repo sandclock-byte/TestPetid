b6cd83a7c580f343cc518525daa268940e31babb
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-12-03 15:22:42 -0600

    Se está implementando con rgba

--- src/utils/base64ToPixels.js
+++ src/utils/base64ToPixels.js
@@ -3,11 +3,22 @@ import PNGReader from '../utils/png';
 
 let uInt8ClampedArray = [];
 export async function base64toPixels(base64) {
-  
+
   base64ToUInt8ClampedArray(base64)
   await delay(1);
-  
-  console.log(uInt8ClampedArray);
+
+  // console.log(uInt8ClampedArray);
+
+  // let arr = uInt8ClampedArray.slice(0, 4);
+
+  // console.log(rGBAToInt(arr));
+  let pixelInt = [];
+  for(let i = 4; i < uInt8ClampedArray.length + 1; i += 4) {
+    let arr = uInt8ClampedArray.slice(i-4, i);
+    pixelInt.push(rGBAToInt(arr));
+  }
+
+  console.log(pixelInt);
 
 }
 
@@ -22,3 +33,12 @@ function base64ToUInt8ClampedArray(base64) {
 }
 
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const rGBAToInt = (rGBA) => {
+  let r = rGBA[0] & 0XFF;
+  let g = rGBA[1] & 0XFF;
+  let b = rGBA[2] & 0XFF;
+  let a = rGBA[3] & 0XFF;
+
+  return (r << 240) + (g << 16) + (b << 8) + (a);
+ }
