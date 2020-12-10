818796b8b4ef0809bf07c07963b1959c9326d2ef
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-12-09 19:13:20 -0600

    Se está agregando diseño a la pantalla Imagen

--- src/components/Imagen.js
+++ src/components/Imagen.js
@@ -1,12 +1,48 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 
 export default function Imagen() {
     return (
-        <View>
-            <Text>Pantalla Imagen</Text>
-        </View>
+        <>
+            <View style={styles.content}>
+                <Text style={styles.text}>Toma una Foto</Text>
+                <Text style={styles.text}>o</Text>
+                <Text style={styles.text}>Selecciona una Imagen</Text>
+            </View>
+
+            <View style={{alignItems: 'center'}}>
+                <TouchableOpacity onPress={() => { }}>
+                    <View style={styles.cameraButton}>
+                        <Image
+                            source={require('../assets/Imagen/cameraIcon.png')}
+                        />
+                    </View>
+                </TouchableOpacity>
+            </View>
+
+        </>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    content: {
+        marginTop: '5%',
+        width: '100%',
+        alignItems: 'center'
+    },
+
+    text: {
+        color: '#979DAC',
+        fontSize: 22,
+    },
+
+    cameraButton: {
+        marginTop: '5%',
+        backgroundColor: '#33415c',
+        borderRadius: 20,
+        paddingVertical: 10,
+        paddingHorizontal: 25,
+        alignItems: 'center',
+        width: '100%',
+    },
+})
